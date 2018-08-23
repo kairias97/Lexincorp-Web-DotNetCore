d6171eb5db7d42fdf9fb11fa81ad15817993a1c4
kairias97 <kevin.irias@inaxir.com>
2018-08-22 18:24:35 -0600

    Agregada funcionalidad de administración y edición de gastos

--- LexinCorpApp/Controllers/ExpenseController.cs
+++ LexinCorpApp/Controllers/ExpenseController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using LexincorpApp.Models;
+using LexincorpApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LexincorpApp.Controllers
@@ -10,7 +11,8 @@ namespace LexincorpApp.Controllers
     public class ExpenseController : Controller
     {
         private readonly IExpenseRepository _expensesRepo;
-
+        public int PageSize = 5;
+        
         public ExpenseController(IExpenseRepository expensesRepository)
         {
             this._expensesRepo = expensesRepository;
@@ -29,6 +31,43 @@ namespace LexincorpApp.Controllers
             }
             _expensesRepo.Save(expense);
             return RedirectToAction(nameof(New), new { added = true });
+        }
+
+        public IActionResult Admin(string filter, int pageNumber = 1)
+        {
+            Func<Expense, bool> filterFunction = e => String.IsNullOrEmpty(filter) || e.SpanishDescription.Contains(filter) || e.EnglishDescription.Contains(filter);
+            ExpenseListViewModel vm = new ExpenseListViewModel
+            {
+                CurrentFilter = filter,
+                Expenses = _expensesRepo.Expenses.Where(filterFunction),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = pageNumber,
+                    ItemsPerPage = this.PageSize,
+                    TotalItems = _expensesRepo.Expenses.Count(filterFunction)
+                }
+            };
+            return View(vm);
+        }
+        public IActionResult Edit(int id, bool? updated)
+        {
+            ViewBag.UpdatedExpense = updated;
+            Expense expense = _expensesRepo.Expenses.Where(e => e.Id == id).FirstOrDefault();
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+        [HttpPost]
+        public IActionResult Edit(Expense expense)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(expense);
+            }
+            _expensesRepo.Save(expense);
+            return RedirectToAction(nameof(Edit), new { updated = true });
         }
     }
 }
---
+++ LexinCorpApp/Models/ViewModels/ExpenseListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models.ViewModels
+{
+    public class ExpenseListViewModel
+    {
+        public string CurrentFilter { get; set; }
+        public IEnumerable<Expense> Expenses { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
