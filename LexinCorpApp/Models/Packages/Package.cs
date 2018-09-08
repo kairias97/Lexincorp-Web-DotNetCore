01857ad1a6a6487c5f660413e0102c3b35180e6f
kairias97 <kevin.irias@inaxir.com>
2018-09-07 23:15:07 -0600

    Agregado módulo de administración de retainers y de paquetes.

---
+++ LexinCorpApp/Models/Packages/Package.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El título del paquete es requerido")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "La descripción es requerida")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "La fecha del paquete es requerida")]
+        public DateTime? RealizationDate { get; set; }
+        [Required(ErrorMessage = "El monto acordado de honorarios es requerido")]
+        public decimal Amount { get; set; }
+        public bool IsFinished { get; set; } = false;
+        [Required(ErrorMessage = "No se ha seleccionado al cliente al cual se asociará el paquete")]
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; }
+        public int CreatorUserId { get; set; }
+        public virtual User Creator { get; set; }
+    }
+}
