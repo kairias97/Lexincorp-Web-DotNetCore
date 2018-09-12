cbcf0f5d531a581907d23f6eed0156b18a1ca85f
kairias97 <kevin.irias@inaxir.com>
2018-09-11 19:02:57 -0600

    Agregado el controlador de suscripciones y el módulo de administración, edición y eliminado.

--- LexinCorpApp/Controllers/RetainerSubscriptionController.cs
+++ LexinCorpApp/Controllers/RetainerSubscriptionController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using LexincorpApp.Infrastructure;
 using LexincorpApp.Models;
 using LexincorpApp.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -14,13 +15,13 @@ namespace LexincorpApp.Controllers
     {
         public int PageSize = 10;
         private readonly IRetainerRepository _retainerRepository;
-        private readonly IRetainerSubscriptionRepository _retainersubscriptionRepository;
+        private readonly IRetainerSubscriptionRepository _retainerSubscriptionRepository;
 
         public RetainerSubscriptionController(IRetainerRepository retainerRepository,
             IRetainerSubscriptionRepository retainerSubscriptionRepository)
         {
             _retainerRepository = retainerRepository;
-            _retainersubscriptionRepository = retainerSubscriptionRepository;
+            _retainerSubscriptionRepository = retainerSubscriptionRepository;
         }
         public IActionResult New()
         {
@@ -56,13 +57,13 @@ namespace LexincorpApp.Controllers
             var currentUserIdClaim = HttpContext.User.Claims.Where(c => c.Type == ClaimTypes.NameIdentifier).First();
             //The creator is the current user
             RetainerSubscription.CreatorId = Convert.ToInt32(currentUserIdClaim.Value);
-            _retainersubscriptionRepository.Save(RetainerSubscription);
+            _retainerSubscriptionRepository.Save(RetainerSubscription);
             TempData["added"] = true;
             return RedirectToAction(nameof(New));
         }
         public JsonResult GetByClient(int clientId)
         {
-            var results = _retainersubscriptionRepository.Subscriptions
+            var results = _retainerSubscriptionRepository.Subscriptions
                 .Include(s => s.Retainer)
                 .Where(rs => rs.ClientId == clientId)
                 .Select(rs => new {
@@ -74,6 +75,65 @@ namespace LexincorpApp.Controllers
                     clientId = rs.ClientId
                 });
             return Json(results);
+        }
+
+        public IActionResult Admin(string filter, int pageNumber = 1)
+        {
+            TempData["filter"] = filter;
+            ViewBag.Deleted = TempData["deleted"];
+            Func<RetainerSubscription, bool> filterFunction = rs => String.IsNullOrEmpty(filter) || rs.Client.Name.CaseInsensitiveContains(filter);
+            RetainerSubscriptionListViewModel vm = new RetainerSubscriptionListViewModel
+            {
+                CurrentFilter = filter,
+                Subscriptions = _retainerSubscriptionRepository.Subscriptions
+                    .Include(rs => rs.Client)
+                    .Include(rs => rs.Retainer)
+                    .Where(filterFunction)
+                    .OrderBy(rs => rs.Retainer.SpanishName)
+                    .Skip((pageNumber - 1) * pageNumber)
+                    .Take(pageNumber),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = pageNumber,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _retainerSubscriptionRepository.Subscriptions.Count(filterFunction)
+                }
+            };
+            return View(vm);
+        }
+        public IActionResult Edit(int id)
+        {
+            var subscription = _retainerSubscriptionRepository.Subscriptions
+                .Include(s => s.Client)
+                .Include(s => s.Retainer)
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+            if (subscription == null)
+            {
+                return NotFound();
+            }
+
+            ViewBag.UpdatedSubscription = TempData["updated"];
+            return View(subscription);
+        }
+        [HttpPost]
+        public IActionResult Edit(RetainerSubscription retainerSubscription)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(retainerSubscription);
+            }
+            _retainerSubscriptionRepository.Save(retainerSubscription);
+            TempData["updated"] = true;
+            return RedirectToAction(nameof(Edit), new { id = retainerSubscription.Id });
+        }
+        [HttpPost]
+        public IActionResult Delete(int retainerSubscriptionId)
+        {
+            
+            _retainerSubscriptionRepository.Delete(retainerSubscriptionId);
+            TempData["deleted"] = true;
+            return RedirectToAction(nameof(Admin), new { filter = TempData["filter"], pageNumber = 1});
         }
     }
 }
---
+++ LexinCorpApp/Models/ViewModels/RetainerSubscriptionListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models.ViewModels
+{
+    public class RetainerSubscriptionListViewModel
+    {
+        public string CurrentFilter { get; set; }
+        public IEnumerable<RetainerSubscription> Subscriptions { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
