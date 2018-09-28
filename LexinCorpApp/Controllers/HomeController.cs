6b7c20e0df1cc697b3cf43a5e2bdd87e8ab4380e
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-09-28 17:22:02 -0600

    Se agrega Dashboard funcional

--- LexinCorpApp/Controllers/HomeController.cs
+++ LexinCorpApp/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using LexincorpApp.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,11 +13,27 @@ namespace LexincorpApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
+        private readonly IActivityRepository _activityRepo;
+        private readonly IAttorneyRepository _attorneyRepo;
+        public HomeController(IActivityRepository _activityRepo, IAttorneyRepository _attorneyRepo)
+        {
+            this._activityRepo = _activityRepo;
+            this._attorneyRepo = _attorneyRepo;
+        }
         [Authorize]
         public IActionResult Index()
         {
-            return View();
+            DashboardViewModel viewModel = new DashboardViewModel();
+            var user = HttpContext.User;
+            var id = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+            var list = _activityRepo.Activities.OrderByDescending(a => a.RealizationDate).Where(a => a.CreatorId == Convert.ToInt32(id)).Take(5).ToList();
+            var vacations = _attorneyRepo.Attorneys.Where(a => a.UserId == Convert.ToInt32(id)).FirstOrDefault().VacationCount;
+            var listHours = _activityRepo.Activities.Where(a => a.RealizationDate.Year == DateTime.Now.Year && a.RealizationDate.Month == DateTime.Now.Month && a.CreatorId == Convert.ToInt32(id)).ToList();
+            var hoursWorked = listHours.Sum(h => h.HoursWorked);
+            viewModel.Vacations = Math.Round(vacations,2);
+            viewModel.Activities = list;
+            viewModel.HoursWorked = Math.Round(hoursWorked);
+            return View(viewModel);
         }
     }
 }
---
+++ LexinCorpApp/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models
+{
+    public class DashboardViewModel
+    {
+        public decimal HoursWorked { get; set; }
+        public decimal Vacations { get; set; }
+        public ICollection<Activity> Activities { get; set; }
+    }
+}
