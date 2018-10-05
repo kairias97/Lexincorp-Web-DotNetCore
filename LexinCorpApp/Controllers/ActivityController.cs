0ce5d5634136eff57d880bf3712b5189f3de334b
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-10-05 17:33:35 -0600

    Se agrega reporte de actividades

--- LexinCorpApp/Controllers/ActivityController.cs
+++ LexinCorpApp/Controllers/ActivityController.cs
@@ -10,6 +10,10 @@ using Microsoft.AspNetCore.Authorization;
 using LexincorpApp.Infrastructure;
 using LexincorpApp.Models.ExternalServices;
 using System.Security.Claims;
+using Syncfusion.Report;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.ReportWriter;
+using System.IO;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace LexincorpApp.Controllers
@@ -24,10 +28,12 @@ namespace LexincorpApp.Controllers
         private readonly IRetainerRepository _retainerRepo;
         private readonly IClientRepository _clientRepo;
         private readonly IActivityRepository _activityRepo;
+        private readonly IAttorneyRepository _attorneyRepo;
         public int PageSize = 5;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _hostingEnvironment;
         public ActivityController(IItemRepository _itemsRepo, IExpenseRepository _expenseRepo, ICategoryRepository _categoryRepo,
             IServiceRepository _serviceRepo, IPackageRepository _packageRepo, IRetainerRepository _retainerRepo, IClientRepository _clientRepo,
-            IActivityRepository _activityRepo)
+            IActivityRepository _activityRepo, IAttorneyRepository attorneyRepository, Microsoft.AspNetCore.Hosting.IHostingEnvironment hostingEnvironment)
         {
             this._itemsRepo = _itemsRepo;
             this._expenseRepo = _expenseRepo;
@@ -37,6 +43,8 @@ namespace LexincorpApp.Controllers
             this._retainerRepo = _retainerRepo;
             this._clientRepo = _clientRepo;
             this._activityRepo = _activityRepo;
+            this._attorneyRepo = attorneyRepository;
+            _hostingEnvironment = hostingEnvironment;
         }
         public IActionResult Index()
         {
@@ -87,6 +95,52 @@ namespace LexincorpApp.Controllers
                 TotalItems = _activityRepo.Activities.Where(a => a.CreatorId == Convert.ToInt32(id)).Count(filterFunction)
             };
             return View(viewModel);
+        }
+        [Authorize]
+        public IActionResult Report()
+        {
+            ActivityReportViewModel viewModel = new ActivityReportViewModel();
+            viewModel.Attorneys = _attorneyRepo.Attorneys;
+            viewModel.NewActivityReport = new NewActivityReport();
+            return View(viewModel);
+        }
+        [Authorize]
+        [HttpPost]
+        public IActionResult Report(NewActivityReport newActivityReport)
+        {
+            var list = new List<Activity>();
+            list = _activityRepo.Activities.Include(a => a.Service).ThenInclude(s => s.Category)
+                .Include(a => a.Client)
+                .Where(a => a.RealizationDate >= newActivityReport.InitialDate && a.RealizationDate <= newActivityReport.FinalDate
+                    && (newActivityReport.UserId == null || a.CreatorId == newActivityReport.UserId)
+                    && (newActivityReport.ActivityType == null || a.ActivityType == newActivityReport.ActivityType)
+                ).ToList();
+            var actividades = list.Select(a => new
+            {
+                activityClient = a.Client.Name,
+                activityService = a.Service.Name,
+                activityCategory = a.Service.Category.Name,
+                activityHoursWorked = Math.Round(a.HoursWorked, 2),
+                activityAssociatedTo = a.ActivityType == ActivityTypeEnum.Hourly ? "Horario" : a.ActivityType == ActivityTypeEnum.Item ?
+                $"Ítem - {a.Item.Name}" : a.ActivityType == ActivityTypeEnum.Package ? $"Paquete - {a.Package.Name}" : a.ActivityType == ActivityTypeEnum.Retainer ?
+                $"Retainer - {a.BillableRetainer.Name}" : "",
+                activityDate = a.RealizationDate
+            }).ToList();
+            string basePath = _hostingEnvironment.ContentRootPath;
+            string fullPath = basePath + @"/Reports/Activities.rdlc";
+            FileStream inputStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            ReportDataSourceCollection dataSources = new ReportDataSourceCollection();
+            dataSources.Add(new ReportDataSource { Name = "ActivitiesDataSet", Value = actividades });
+
+            Syncfusion.ReportWriter.ReportWriter writer = new Syncfusion.ReportWriter.ReportWriter(inputStream, dataSources);
+            //writer.DataSources = dataSources;
+            writer.ReportProcessingMode = ProcessingMode.Local;
+            MemoryStream memoryStream = new MemoryStream();
+            writer.Save(memoryStream, WriterFormat.PDF);
+            memoryStream.Position = 0;
+            FileStreamResult fileStreamResult = new FileStreamResult(memoryStream, "application/pdf");
+            fileStreamResult.FileDownloadName = $"ReporteDeActividadesDel{newActivityReport.InitialDate.ToString("ddMMyyyy")}Al{newActivityReport.FinalDate.ToString("ddMMyyyy")}.pdf";
+            return fileStreamResult;
         }
     }
 }
