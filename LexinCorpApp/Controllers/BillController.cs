e18418b3d69ecf7a60a147dc1ba9064983c7cebc
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-10-06 17:49:11 -0600

    Se agrega form para la prefacturacion

---
+++ LexinCorpApp/Controllers/BillController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LexincorpApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LexincorpApp.Controllers
+{
+    public class BillController : Controller
+    {
+        private readonly IItemRepository _itemRepo;
+        public BillController(IItemRepository itemRepository)
+        {
+            this._itemRepo = itemRepository;
+        }
+        [Authorize]
+        public IActionResult PreBilling()
+        {
+            NewPreBillViewModel viewModel = new NewPreBillViewModel();
+            viewModel.Items = _itemRepo.Items.ToList();
+            return View(viewModel);
+        }
+    }
+}
---
+++ LexinCorpApp/Models/ViewModels/NewPreBillViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models { 
+    public class NewPreBillViewModel
+    {
+        public IEnumerable<Item> Items { get; set; }
+        public bool IsEnglish { get; set; }
+        public bool IsClientSelected { get; set; }
+        public string ClientName { get; set; }
+        public int ClientId { get; set; }
+    }
+}
