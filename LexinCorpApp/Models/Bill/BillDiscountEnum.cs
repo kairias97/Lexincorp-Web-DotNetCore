c544a01db9d8f945cc20092c292b19b928ecb85f
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-10-06 17:49:11 -0600

    Se agrega form para la prefacturacion

---
+++ LexinCorpApp/Models/Bill/BillDiscountEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models
+{
+    public enum BillDiscountEnum: int
+    {
+        [Display(Name = "Porcentual")]
+        Percentage = 1,
+        [Display(Name = "Neto")]
+        Net = 2
+    }
+}
