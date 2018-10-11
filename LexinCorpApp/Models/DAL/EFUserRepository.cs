bc5beb357f27f05799d288ad8a3d39ac510857cf
kairias97 <kevin.irias@inaxir.com>
2018-10-10 22:47:57 -0600

    Agregada funcionalidad de restauración de clave, actualizado los títulos e implementado select2js.

--- LexinCorpApp/Models/DAL/EFUserRepository.cs
+++ LexinCorpApp/Models/DAL/EFUserRepository.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using LexincorpApp.Models.ExternalServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace LexincorpApp.Models
 {
     public class EFUserRepository : IUserRepository
     {
         private ApplicationDbContext context;
-        public EFUserRepository(ApplicationDbContext ctx)
+        private readonly IGuidManager _guidManager;
+        private readonly ICryptoManager _cryptoManager;
+        private readonly IMailSender _mailSender; 
+        public EFUserRepository(ApplicationDbContext ctx,
+            IGuidManager guidManager,
+            ICryptoManager cryptoManager,
+            IMailSender mailSender)
         {
             this.context = ctx;
+            _guidManager = guidManager;
+            _cryptoManager = cryptoManager;
+            _mailSender = mailSender;
         }
         public IQueryable<User> Users { get => context.Users; }
 
@@ -46,6 +57,24 @@ namespace LexincorpApp.Models
             context.Update(user);
             context.Entry<User>(user).Property(x => x.Password).IsModified = true;
             context.SaveChanges();
+        }
+
+        public void ResetPassword(int attorneyId)
+        {
+            User user = context.Users
+                .Include(u => u.Attorney)
+                .Where(u => u.Attorney.Id == attorneyId)
+                .First();
+            string guidGenerated = _guidManager.GenerateGuid();
+            string passwordRestored = guidGenerated.Substring(guidGenerated.Length - 12, 12);
+            string passwordHashed = _cryptoManager.HashString(passwordRestored);
+            user.Password = passwordHashed;
+            context.SaveChanges();
+            //Envío de password sin hash al usuario
+            string emailBody = $"Se le ha restablecido su clave de acceso a la aplicación Lexincorp Nicaragua Web," +
+                $"y su clave de acceso nueva es *{passwordRestored}*. Se recomienda cambiar su clave una vez ingrese nuevamente al sistema." +
+                $" \n**Este es un mensaje autogenerado por el sistema, favor no responder**";
+            _mailSender.SendMail(user.Attorney.Email, "Contraseña restablecida para aplicación Lexincorp Nicaragua Web", emailBody);
         }
     }
 }
