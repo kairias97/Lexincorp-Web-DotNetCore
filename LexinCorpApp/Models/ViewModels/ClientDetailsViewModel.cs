d036417676dc05c62c7dcc6de4476c24e6da1b62
Dustin Espinoza <dustin.espinoza.htz@gmail.com>
2018-11-04 15:44:31 -0600

    Se completa el punto 7

---
+++ LexinCorpApp/Models/ViewModels/ClientDetailsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using LexincorpApp.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LexincorpApp.Models
+{
+    public class ClientDetailsViewModel
+    {
+        public Client Client { get; set; }
+        public IEnumerable<RetainerSubscription> RetainerSubscriptions { get; set; }
+        public IEnumerable<Package> Packages { get; set; }
+        public IEnumerable<ClientType> ClientTypes { get; set; }
+        public IEnumerable<BillingMode> BillingModes { get; set; }
+        public IEnumerable<DocumentDeliveryMethod> DocumentDeliveryMethods { get; set; }
+    }
+}
